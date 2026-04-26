2aa0125afda954c85e267be8e582a92c60065ce6
250 <ghgh2505380@gmail.com>
2026-04-26 23:36:01 +0900

    feat(lecture): LectureService 메서드 추가(#5)

--- src/main/java/com/goggles/lecture_service/application/lecture/LectureService.java
+++ src/main/java/com/goggles/lecture_service/application/lecture/LectureService.java
@@ -1,16 +1,21 @@
 package com.goggles.lecture_service.application.lecture;
 
+import java.util.UUID;
+
 import com.goggles.common.pagination.CommonPageRequest;
 import com.goggles.common.pagination.CommonPageResponse;
+import com.goggles.lecture_service.application.lecture.command.dto.LectureCreateCommand;
+import com.goggles.lecture_service.application.lecture.command.dto.LectureCreateResult;
 import com.goggles.lecture_service.application.lecture.query.dto.LectureDetail;
 import com.goggles.lecture_service.application.lecture.query.dto.LectureSummary;
 import com.goggles.lecture_service.domain.lecture.LectureSearchCondition;
-import java.util.UUID;
 
 public interface LectureService {
 
-  CommonPageResponse<LectureSummary> getLectures(
-      LectureSearchCondition condition, CommonPageRequest pageRequest);
+	CommonPageResponse<LectureSummary> getLectures(
+		LectureSearchCondition condition, CommonPageRequest pageRequest);
 
-  LectureDetail getLectureDetail(UUID lectureId);
+	LectureDetail getLectureDetail(UUID lectureId);
+
+	LectureCreateResult createLecture(LectureCreateCommand command);
 }
