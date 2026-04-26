ce6af9bddf63a2f0241376f4edcc93ac175981d6
250 <ghgh2505380@gmail.com>
2026-04-26 23:33:19 +0900

    feat(lecture): LectureCommandService 구현(#5)

---
+++ src/main/java/com/goggles/lecture_service/application/lecture/command/service/LectureCommandServiceImpl.java
@@ -0,0 +1,37 @@
+package com.goggles.lecture_service.application.lecture.command.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goggles.lecture_service.application.lecture.command.dto.LectureCreateCommand;
+import com.goggles.lecture_service.application.lecture.command.dto.LectureCreateResult;
+import com.goggles.lecture_service.domain.lecture.Lecture;
+import com.goggles.lecture_service.domain.lecture.repository.LectureRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LectureCommandServiceImpl implements LectureCommandService {
+
+	private final LectureRepository lectureRepository;
+
+	@Override
+	public LectureCreateResult createLecture(LectureCreateCommand command) {
+		Lecture lecture =
+			Lecture.create(
+				command.instructorId(),
+				command.instructorName(),
+				command.category(),
+				command.title(),
+				command.subtitle(),
+				command.description(),
+				command.durationPolicy(),
+				command.price()
+			);
+
+		Lecture saved = lectureRepository.save(lecture);
+		return LectureCreateResult.from(saved);
+	}
+}
