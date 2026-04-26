f5ce5524815c623213e92793fc75ab07e49c9489
250 <ghgh2505380@gmail.com>
2026-04-26 22:06:03 +0900

    fix(lecture): spotless 에러 해결 (#4)

--- src/main/java/com/goggles/lecture_service/infrastructure/lecture/repository/LectureJpaRepository.java
+++ src/main/java/com/goggles/lecture_service/infrastructure/lecture/repository/LectureJpaRepository.java
@@ -1,20 +1,18 @@
 package com.goggles.lecture_service.infrastructure.lecture.repository;
 
+import com.goggles.lecture_service.domain.lecture.Lecture;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.goggles.lecture_service.domain.lecture.Lecture;
-
 public interface LectureJpaRepository extends JpaRepository<Lecture, UUID> {
 
-	List<Lecture> findAllByInstructor_InstructorId(UUID instructorId);
+  List<Lecture> findAllByInstructor_InstructorId(UUID instructorId);
 
-	// soft delete 필터 우회를 위해 native query 사용
-	@Query(value = "SELECT * FROM lecture.p_lecture WHERE id = :id", nativeQuery = true)
-	Optional<Lecture> findByIdIncludeDeleted(@Param("id") UUID id);
+  // soft delete 필터 우회를 위해 native query 사용
+  @Query(value = "SELECT * FROM lecture.p_lecture WHERE id = :id", nativeQuery = true)
+  Optional<Lecture> findByIdIncludeDeleted(@Param("id") UUID id);
 }
