4c5969fae848fb24fb3618ea408f83c804774d95
250 <ghgh2505380@gmail.com>
2026-04-29 00:14:14 +0900

    feat(enrollment): 본인 소유 검증 예외 추가 (#17)

---
+++ src/main/java/com/goggles/lecture_service/domain/enrollment/exception/EnrollmentNotOwnedException.java
@@ -0,0 +1,10 @@
+package com.goggles.lecture_service.domain.enrollment.exception;
+
+import com.goggles.common.exception.BadRequestException;
+
+public class EnrollmentNotOwnedException extends BadRequestException {
+
+	public EnrollmentNotOwnedException(EnrollmentErrorCode errorCode) {
+		super(errorCode.getMessage());
+	}
+}
